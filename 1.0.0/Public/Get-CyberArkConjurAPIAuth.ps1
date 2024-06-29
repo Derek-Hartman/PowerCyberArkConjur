7c75158bd1370b6f62f543d3fc6d58695feee190
Derek-Hartman <57258132+Derek-Hartman@users.noreply.github.com>
2024-06-29 00:11:21 -0400

    First Commit

---
+++ 1.0.0/Public/Get-CyberArkConjurAPIAuth.ps1
@@ -0,0 +1,44 @@
+﻿<#
+.Synopsis
+    Uses API token to Auth to CyberArk Conjur Cloud
+
+.EXAMPLE 
+    Get-CyberArkConjurAPIAuth -ConjurCloudURL "XXXX.cyberark.cloud" -WorkflowID "data/XXXXX" -APIKey "APIKey"
+
+.NOTES
+    Modified by: Derek Hartman
+    Date: 6/12/2024
+
+#>
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Function Get-CyberArkConjurAPIAuth {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $True,
+            HelpMessage = "Enter your Conjur Cloud URL.")]
+        [string[]]$ConjurCloudURL,
+
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $True,
+            HelpMessage = "Enter your Workflow ID.")]
+        [string[]]$WorkflowID,
+
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $True,
+            HelpMessage = "Enter your API Key.")]
+        [string[]]$APIKey
+    )
+
+    $Header = @{
+        'Content-Type'     = 'application/json';
+        'Accept-Encoding'  = 'base64';
+    }
+
+    $Body = "$APIKey"
+	
+	$Uri = "https://$($ConjurCloudURL)/api/authn/conjur/host/$($WorkflowID)/authenticate"
+
+    $Response = Invoke-RestMethod -Uri $Uri -Method Post -Headers $Header -Body $Body
+    Write-Output $Response
+}
---
+++ 1.0.0/Public/Get-CyberArkConjurSecret.ps1
@@ -0,0 +1,42 @@
+﻿<#
+.Synopsis
+    After Token is generated. Use the token to connect again and retrieve the secret.
+
+.EXAMPLE 
+    Get-CyberArkConjurSecret -ConjurCloudURL "XXXX.cyberark.cloud" -SecretID "data/vault/xxxxx/xxxxxx/password" -Token "Token"
+
+.NOTES
+    Modified by: Derek Hartman
+    Date: 6/12/2024
+
+#>
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Function Get-CyberArkConjurSecret {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $True,
+            HelpMessage = "Enter your Conjur Cloud URL.")]
+        [string[]]$ConjurCloudURL,
+
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $True,
+            HelpMessage = "Enter your Secret ID.")]
+        [string[]]$SecretID,
+
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $True,
+            HelpMessage = "Enter your API Key.")]
+        [string[]]$Token
+    )
+
+    $Header = @{
+        'Content-Type'     = 'application/json';
+        'Authorization'  = "Token token=`"$($Token)`"";
+    }
+	
+	$Uri = "https://$($ConjurCloudURL)/api/secrets/conjur/variable/$($SecretID)"
+
+    $Response = Invoke-RestMethod -Uri $Uri -Method Get -Headers $Header
+    Write-Output $Response
+}
